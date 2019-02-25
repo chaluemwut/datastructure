f1e5ebc473ef6da2b99c80aa3c425af5d1bf6d88
chaluemwut <chaluemwut@users.noreply.github.com>
2019-02-25 15:46:26 +0700

    Update Main.java

--- src/main/java/datastructure/binarytree/Main.java
+++ src/main/java/datastructure/binarytree/Main.java
@@ -6,15 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         BSTree bst = new BSTree();
+        //add
         bst.add(10);
         bst.add(5);
         bst.add(30);
         bst.add(40);
-//        bst.remove(5);
+        //remove
+        bst.remove(5);
         System.out.println(bst.get(35));
         System.out.println(bst.getMin());
-//        Integer[] data = new Integer[]{2, 3, 4, 1, 10};
-//        BSTree.treeSort(data);
-//        System.out.println(Arrays.toString(data));
+        //tree sort
+        Integer[] data = new Integer[]{2, 3, 4, 1, 10};
+        BSTree.treeSort(data);
+        System.out.println(Arrays.toString(data));
     }
 }
